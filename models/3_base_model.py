4d77fc90c9fb9fe83decc2bb9fa59563c90af231
wolf-coder <cuore.fakhri@gmail.com>
2020-06-26 17:46:46 +0100

    base_model

---
+++ models/3_base_model.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+import uuid
+from datetime import datetime, timezone
+"""
+"""
+
+
+class BaseModel:
+    """
+    """
+    def __init__(self):
+        """
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __setattr__(self, name, value):
+        """
+        """
+        self.__dict__[name] = value
+        try:
+            object.__setattr__(self, updated_at, datetime.now())
+        except:
+            pass
+
+    def __str__(self):
+        """
+        """
+        return ("[{}] ({}) {}".format(
+            self.__class__.__name__,
+            self.id,
+            self.__dict__))
+
+    def save(self):
+        """
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        """
+        dict2 = dict(self.__dict__)
+        dict2["__class__"] = self.__class__.__name__
+        dict2["updated_at"] = self.updated_at.isoformat()
+        dict2["created_at"] = self.created_at.isoformat()
+        return dict2
+
