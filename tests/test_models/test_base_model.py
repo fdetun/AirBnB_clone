a6134a3d79b9e56b7ad3c4d5b26c28adabfb2046
fdetun <fouedads@gmail.com>
2020-06-30 19:52:04 +0100

    fde

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,4 +1,30 @@
 #!/usr/bin/python3
-"""testing"""
+"""test of file model file"""
 import unittest
 from models.base_model import BaseModel
+from datetime import datetime
+import uuid
+
+
+class TestBaseModel(unittest.TestCase):
+    """BaseModel test"""
+
+    def test_1(self):
+        """id"""
+        a = BaseModel()
+        b = BaseModel()
+        self.assertNotEqual(a.id, b.id)
+
+    def test_2(self):
+        """str"""
+        d = BaseModel()
+        self.assertIsInstance(d.__str__(), str)
+
+    def test_4(self):
+        """dict"""
+        d = BaseModel()
+        self.assertIsInstance(d.to_dict(), dict)
+
+
+if __name__ == '__main__':
+    unittest.main()
