0acce31b96c1e06c923edfa0dbddb108643b73ac
fdetun <fouedads@gmail.com>
2020-06-30 11:58:00 +0100

    fix req

--- models/base_model.py
+++ models/base_model.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
+"""BaseModel Class File"""
 import uuid
-from datetime import datetime, timezone
+from datetime import datetime
 import models
 
 
 class BaseModel:
+    """BaseModel Class"""
 
     def __init__(self, *args, **kwargs):
+        """init func
+        args:
+        """
         if kwargs:
             for key, value in kwargs.items():
                 if key != "__class__":
@@ -22,14 +27,27 @@ class BaseModel:
             models.storage.new(self)
 
     def __str__(self):
+        """str func
+        return str
+        """
+
         return ("[{}] ({}) {}".format(
             self.__class__.__name__, self.id, self.__dict__))
 
     def save(self):
+        """save func
+        args:
+        self
+        """
+
         self.updated_at = datetime.now()
         models.storage.__class__.save(self)
 
     def to_dict(self):
+        """to_dict func
+        args:
+        self
+        """
         dict2 = dict(self.__dict__)
         dict2["__class__"] = self.__class__.__name__
         dict2["updated_at"] = self.updated_at.isoformat()
