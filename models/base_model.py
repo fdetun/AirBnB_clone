22d91e6a52a645f032e4232c07b0fb3dcbccbd18
wolf-coder <cuore.fakhri@gmail.com>
2020-06-30 12:23:29 +0100

    fixing

--- models/base_model.py
+++ models/base_model.py
@@ -1,55 +1,66 @@
 #!/usr/bin/python3
-"""BaseModel Class File"""
 import uuid
-from datetime import datetime
+from datetime import datetime, timezone, timedelta
 import models
+# print("{}".format(id(models.storage)))
+"""
+"""
 
 
 class BaseModel:
-    """BaseModel Class"""
-
+    """
+    """
     def __init__(self, *args, **kwargs):
-        """init func
-        args:
+        """
         """
         if kwargs:
-            for key, value in kwargs.items():
-                if key != "__class__":
-                    if key in ["created_at", "updated_at"]:
-                        setattr(self, key, datetime.strptime(
-                            value, "%Y-%m-%dT%H:%M:%S.%f"))
-                    else:
-                        setattr(self, key, value)
+            for key, val in kwargs.items():
+                if key in ["created_at", "updated_at"]:
+                    setattr(self, key, To_Date(val))
+                elif key != '__class__':
+                    setattr(self, key, val)
         else:
-            self.updated_at = datetime.now()
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            models.storage.new(self)
+            self.updated_at = datetime.now()
+            """
+            call to the method new(self) on storage
+            """
+            models.storage.__class__.new(models.storage, self)
 
     def __str__(self):
-        """str func
-        return str
         """
-
+        """
         return ("[{}] ({}) {}".format(
-            self.__class__.__name__, self.id, self.__dict__))
+            self.__class__.__name__,
+            self.id,
+            self.__dict__))
 
     def save(self):
-        """save func
-        args:
-        self
         """
-
+        """
         self.updated_at = datetime.now()
+        """
+        call save(self) method of models.storage
+        -->try Using FileStorage.save(storage)
+        """
         models.storage.__class__.save(self)
 
     def to_dict(self):
-        """to_dict func
-        args:
-        self
+        """
         """
         dict2 = dict(self.__dict__)
         dict2["__class__"] = self.__class__.__name__
         dict2["updated_at"] = self.updated_at.isoformat()
         dict2["created_at"] = self.created_at.isoformat()
         return dict2
+
+
+def To_Date(str_dt):
+    """
+    Function to transform datetime representation to datetime object
+    """
+    Splittet_List = str_dt.split(".")
+    Part1 = datetime.strptime(Splittet_List[0], "%Y-%m-%dT%H:%M:%S")
+    Part2 = int(Splittet_List[1].rstrip("Z"), 10)
+    return Part1 + timedelta(microseconds=Part2)
