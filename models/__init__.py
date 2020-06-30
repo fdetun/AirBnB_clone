d3404980055a1349906f1212792d874a1b218f97
wolf-coder <cuore.fakhri@gmail.com>
2020-06-30 12:24:16 +0100

    one for all

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
-from models.engine.file_storage import FileStorage
-
-storage = FileStorage()
+"""
+"""
+from .engine import file_storage
+# from .base_model import BaseModel
+"""
+create the variable storage, an instance of FileStorage
+"""
+storage = file_storage.FileStorage()
+"""
+call reload() method on this variable
+"""
 storage.reload()
