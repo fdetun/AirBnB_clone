3b64626b39385b2c49aa600e5ccc4f6f7fb3ee2a
wolf-coder <cuore.fakhri@gmail.com>
2020-06-30 22:53:33 +0100

    console.py that contains the entry point of the command interpreter:

--- console.py
+++ console.py
@@ -1,21 +1,36 @@
 #!/usr/bin/python3
+"""
+Contains the entry point of the command interpreter
+"""
+import cmd
 
-import cmd, sys
 
 class HBNBCommand(cmd.Cmd):
+    """
+    Subclass of cmd.Cmd
+    """
     prompt = '(hbnb) '
 
-    def do_quit(self, ino):
+    def do_EOF(self, line):
+        """
+        Method to exit the program.
+        """
         return True
-    def help_quit(self):
-        print('Quit command to exit the program')
-  
-    def help_EOF(self):
-        print('EOF command to exit the program')
-    do_EOF = do_quit
+
+    def do_quit(self, line):
+        """Quit command to exit the program
+        """
+        return True
+
+    def emptyline(self):
+        """
+        emptyline
+        """
+        pass
 
 
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
+    """
+    code executed when not imported
+    """
     HBNBCommand().cmdloop()
