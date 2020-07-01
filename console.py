12cd0a5693f35fd96fa35df2fad6a5682956f0d6
wolf-coder <cuore.fakhri@gmail.com>
2020-07-01 23:57:25 +0100

    updating task 8

--- console.py
+++ console.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
 """
-Contains the entry point of the cmd interpreter
+Contains the entry point of the command interpreter
 """
 import cmd
 from models import storage
 from models.base_model import BaseModel
+from models.user import User
 
-
+Cls = {"BaseModel":BaseModel, "User":User}
 class HBNBCommand(cmd.Cmd):
     """
-    HBNBCommand of cmd.Cmd
+    Subclass of cmd.Cmd
     """
     prompt = '(hbnb) '
-
+    
     def do_EOF(self, line):
         """
-        Method to exit this program.
+        Method to exit the program.
         """
         return True
 
@@ -27,19 +28,19 @@ class HBNBCommand(cmd.Cmd):
 
     def emptyline(self):
         """
-        enter == empty line
+        emptyline
         """
         pass
 
-    def do_create(self, line):
+    def do_create(self,line):
         """
         Creates a new instance of BaseModel.
-        saves it(to the JSON file) and prints the id.
+        saves it (to the JSON file) and prints the id.
         """
-        if line == '':
+        if line is '':
             print("** class name missing **")
-        elif line == "BaseModel":
-            a = BaseModel()
+        elif line in ["BaseModel","User"]:
+            a = Cls[line]()
             print("{}".format(a.id))
             a.save()
         else:
@@ -51,75 +52,71 @@ class HBNBCommand(cmd.Cmd):
         """
         all_objs = storage.all()
         Args_l = line.split()
-        if len(Args_l) == 0:
+        if len(Args_l) ==0:
             print("** class name missing **")
-        elif Args_l[0] != "BaseModel":
+        elif Args_l[0] not in  ["BaseModel","User"]:
             print("** class doesn't exist **")
         elif len(Args_l) == 1:
             print("** instance id missing **")
-        elif '.'.join(("BaseModel", Args_l[1])) not in all_objs.keys():
+        elif '.'.join((Args_l[0], Args_l[1]))  not in all_objs.keys():
             print("** no instance found **")
         else:
-            print(all_objs['.'.join(("BaseModel", Args_l[1]))])
-
+            print(all_objs['.'.join((Args_l[0], Args_l[1]))])
     def do_destroy(self, line):
         """
         Deletes an instance based on the class name and id
-        (save the change into the JS0N file).
+        (save the change into the JSON file).
         """
         all_objs = storage.all()
         Args_l = line.split()
-        if len(Args_l) == 0:
+        if len(Args_l) ==0:
             print("** class name missing **")
-        elif Args_l[0] != "BaseModel":
+        elif Args_l[0] not in ["BaseModel","User"]:
             print("** class doesn't exist **")
         elif len(Args_l) == 1:
             print("** instance id missing **")
-        elif '.'.join(("BaseModel", Args_l[1])) not in all_objs.keys():
+        elif '.'.join((Args_l[0],Args_l[1]))  not in all_objs.keys():
             print("** no instance found **")
         else:
-            del all_objs['.'.join(("BaseModel", Args_l[1]))]
+            del all_objs['.'.join((Args_l[0],Args_l[1]))]
             storage.save()
-
     def do_all(self, line):
         """
         Prints all string representation of all instances based
-        or not on the class name
+        or not on the class name.
         """
         Args_l = line.split()
-        if len(Args_l) == 1 and Args_l[0] != "BaseModel":
+        if len(Args_l) == 1 and Args_l[0] not in ["BaseModel","User"]:
             print("** class doesn't exist **")
         else:
             all_objs = storage.all()
-            print([str(obj) for Key, obj in all_objs.items()])
+            print([str(obj) for  Key ,obj in all_objs.items()])
 
     def do_update(self, line):
         """Updates an instance based on the class name and id by
-        adding or updating attribute (save the change into the JSON file)
+        adding or updating attribute (save the change into the JSON file).
         """
         all_objs = storage.all()
         Args_l = line.split()
         if len(Args_l) == 0:
             print("** class name missing **")
-        elif Args_l[0] != "BaseModel":
+        elif Args_l[0] not in ["BaseModel", "User"]:
             print("** class doesn't exist **")
         elif len(Args_l) == 1:
             print("** instance id missing **")
-        elif '.'.join(("BaseModel", Args_l[1])) not in all_objs.keys():
+        elif '.'.join((Args_l[0],Args_l[1]))  not in all_objs.keys():
             print("** no instance found **")
         elif len(Args_l) == 2:
             print("** attribute name missing **")
         elif len(Args_l) == 3:
             print("** value missing **")
         else:
-            id = '.'.join(("BaseModel", Args_l[1]))
+            id = '.'.join((Args_l[0],Args_l[1]))
             Dict = all_objs[id].to_dict()
-            print("-->{}".format(type(Args_l[3])))
             Dict[Args_l[2]] = Args_l[3].strip('"\'')
-            tmpBase = BaseModel(**Dict)
+            tmpBase = Cls[Args_l[0]](**Dict)
             all_objs[id] = tmpBase
             storage.save()
-
 
 if __name__ == "__main__":
     """
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,40 +1,53 @@
 #!/usr/bin/python3
 """
-file storage File
 """
 import json
+
 from models.base_model import BaseModel
+from models.user import User
 
 
+Cls = {"BaseModel":BaseModel, "User":User}
 class FileStorage:
-    """File Storage class"""
+    """
+    """
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """all func"""
+        """
+        """
         return FileStorage.__objects
 
     def new(self, obj):
-        """new method"""
-        a = obj.__class__.__name__
-        b = obj.id
-        FileStorage.__objects[str(a) + "." + str(b)] = obj
+        """
+        sets in __objects the obj with key <obj class name>.id
+        """
+        FileStorage.__objects['.'.join((
+            obj.__class__.__name__, str(obj.id)))] = obj
 
     def save(self):
-        """save method"""
-        tmp = {}
-        for key, value in FileStorage.__objects.items():
-            tmp[key] = value.to_dict()
-        with open(FileStorage.__file_path, "w", encoding="utf-8") as file:
-            json.dump(tmp, file)
+        """
+        serializes __objects to the JSON file (path: __file_path)
+        __objects must be updated 
+        """
+        Tmp_dict = {}
+        for Key, Val in FileStorage.__objects.items():
+            Tmp_dict[Key] = Val.to_dict()
+        with open(FileStorage.__file_path, "w") as f:
+            json.dump(Tmp_dict, f)
 
     def reload(self):
-        """reload method"""
+        """
+        deserializes the JSON file to __objects.
+        (only if the JSON file (__file_path) exists ;
+        otherwise, do nothing. If the file doesn’t
+        exist, no exception should be raised)
+        """
         try:
-            with open(FileStorage.__file_path) as file:
-                data = json.load(file)
-            for k, i in data.items():
-                FileStorage.__objects[str(k)] = BaseModel(**i)
-        except Exception:
+            with open(FileStorage.__file_path, "r") as f:
+                tmp = json.load(f)
+                for Key, Value in  tmp.items():
+                    FileStorage.__objects[str(Key)] = Cls[Key.split(".")[0]](**Value)
+        except Exception as e:
             pass
