a946c71eaf35b0864c258f9a0cd535257b11ca4a
fdetun <fouedads@gmail.com>
2020-07-01 15:06:14 +0100

    save

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -71,5 +71,21 @@ class TestFileStorage(unittest.TestCase):
         with self.assertRaises(TypeError):
             f = FileStorage(0, "sdsd", 99, 1, 8, {}, 41, 3, 1, 0, 7, [88, 1])
 
+    def test5(self):
+        """test new without arguments"""
+        b = BaseModel()
+        with self.assertRaises(TypeError) as e:
+            FileStorage.save()
+        msg = "save() missing 1 required"
+        self.assertIn(msg, str(e.exception))
+
+    def test6(self):
+        """test save without too"""
+        b = BaseModel()
+        with self.assertRaises(TypeError) as e:
+            FileStorage.save(10, "sdsd")
+        msg = "save() takes 1 positional argument but 2 were given"
+        self.assertIn(msg, str(e.exception))
+
 if __name__ == '__main__':
     unittest.main()
