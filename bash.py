172390a09ae18e9f67364fc6a590484290602c3b
fdetun <fouedads@gmail.com>
2020-07-01 22:19:34 +0100

    fakhri is here

--- bash.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-import subprocess
-bashCommand = "pep8 --statistics -qq models/engine/file_storage.py"
-process = subprocess.run(bashCommand.split(), stdout=subprocess.PIPE)
-print(process.stdout)
