f08841bc8a7d2a5b93638e0c9483d5a831641ac9
wolf-coder <cuore.fakhri@gmail.com>
2020-07-01 23:02:41 +0100

    fakhri

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -33,7 +33,7 @@ class docs(unittest.TestCase):
         self.assertTrue(len(FileStorage.reload.__doc__) > 0)
 
     def test_save(self):
-        """ test """
+        """test"""
         self.assertTrue(len(FileStorage.save.__doc__) > 0)
 
     def test_method_docs(self):
